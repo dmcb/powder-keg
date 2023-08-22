9aa4c6ac3a97b944cef8a1e31350ecf0a7c9eb2e
Derek McBurney <d.mcburney@icloud.com>
2023-08-22 16:16:11 -0600

    Removes orbit controls

--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,12 @@
 "use client";
 
 import { Canvas } from "@react-three/fiber";
-import { OrbitControls } from "@react-three/drei";
 import Network from "components/Network";
 import Board from "components/Board";
 
 export default function Page() {
   return (
     <Canvas shadows={true} camera={{ fov: 10, position: [0, 0, 12] }}>
-      <OrbitControls />
       <Network />
       <Board />
     </Canvas>
