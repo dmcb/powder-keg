9ffb094b2057543a99a12899832d91d4884f3f1b
Derek McBurney <d.mcburney@icloud.com>
2023-08-25 10:30:01 -0600

    Adds web page metadata

--- app/layout.tsx
+++ app/layout.tsx
@@ -7,6 +7,18 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
+      <head>
+        <title>Powder Keg</title>
+        <meta
+          name="description"
+          content="Powder Keg is a pirate battle game made with React Three Fiber."
+        />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <meta
+          property="og:image"
+          content="https://powderkeg.dmcb.dev/solarsystems.webp"
+        />
+      </head>
       <body>{children}</body>
     </html>
   );
