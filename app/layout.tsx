08da1f911e2278846b1f9003896b1344b05656c5
Derek McBurney <d.mcburney@icloud.com>
2023-08-27 08:23:12 -0600

    Adds opengraph image in Next format

--- app/layout.tsx
+++ app/layout.tsx
@@ -14,10 +14,6 @@ export default function RootLayout({
           content="Powder Keg is a pirate battle game made with React Three Fiber."
         />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <meta
-          property="og:image"
-          content="https://powderkeg.dmcb.dev/powderkeg.webp"
-        />
       </head>
       <body>{children}</body>
     </html>
