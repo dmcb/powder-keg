fdab34bf3978d1fec1fb095f1670bb7b3bd856e4
Derek McBurney <d.mcburney@icloud.com>
2024-02-11 08:43:15 -0700

    Updates scoreboard with player names

--- components/ui/Scoreboard.tsx
+++ components/ui/Scoreboard.tsx
@@ -1,24 +1,25 @@
 import React from "react";
+import { usePlayerStore } from "stores/playerStore";
 
 export default function Scoreboard() {
+  const joinedPlayers = usePlayerStore((state) => state.joinedPlayers);
+  const players = [
+    usePlayerStore((state) => state.player0),
+    usePlayerStore((state) => state.player1),
+    usePlayerStore((state) => state.player2),
+    usePlayerStore((state) => state.player3),
+  ].filter((player, index) => index in joinedPlayers);
+
   return (
     <div id="scoreboard">
-      <div id="player0" className="player">
-        <div className="name">Player 1</div>
-        <div className="score">0</div>
-      </div>
-      <div id="player1" className="player">
-        <div className="name">Player 2</div>
-        <div className="score">0</div>
-      </div>
-      <div id="player2" className="player">
-        <div className="name">Player 3</div>
-        <div className="score">0</div>
-      </div>
-      <div id="player3" className="player">
-        <div className="name">Player 4</div>
-        <div className="score">0</div>
-      </div>
+      {players.map((player, index) => {
+        return (
+          <div id="player{index}" className="player">
+            <div className="name">{player.name}</div>
+            <div className="health">{player.health}</div>
+          </div>
+        );
+      })}
     </div>
   );
 }
